25933f46bdc3165736feb5a3494f259de24c03c4
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-12-30 23:32:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,16 +28,12 @@ class Song
     song
   end
     
-  def self.find_by_name(name)
-     @@all.each do |song|
-       if song.name == name
-         return song
-       end 
-     end
-   end 
+   def self.find_by_name(song_name)
+     self.all.detect { |song| song.name == song_name }
+   end
  
-    def self.find_or_create_by_name(name)
-     self.find_by_name(name) || self.create_by_name(name)
+    def self.find_or_create_by_name(song_name)
+     find_by_name(song_name) || create_by_name(song_name)
    end
   
 end
